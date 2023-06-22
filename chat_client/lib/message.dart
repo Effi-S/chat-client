06d776b471a954efbd36a5206fbcb77ceb1a1f7a
efrayims <efrayims@ibm.com>
2023-06-22 21:11:52 +0300

    Initializing chat_client

---
+++ chat_client/lib/message.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart';
+
+class Message {
+  final String username;
+  final String text;
+
+  Message({
+    required this.username,
+    required this.text,
+  });
+
+  factory Message.fromJson(Map<String, dynamic> json) {
+    return Message(
+      username: json['username'],
+      text: json['text'],
+    );
+  }
+}
---
+++ chat_client/lib/message_service.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:chat_client/message.dart';
+
+class MessageService {
+  Future<List<Message>> loadMessages() async {
+    final response =
+        await http.get('http://localhost:8080/api/v1/users/get/all' as Uri);
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+      return data.map((message) => Message.fromJson(message)).toList();
+    } else {
+      throw Exception('Failed to load messages');
+    }
+  }
+}
